1309f4d2a9d9edee809e266cf401c81fac9f0318
adarshhh25 <adarshjha4425@gmail.com>
2025-07-09 22:26:58 +0530

    Changes Done

---
+++ src/lib/aurinko.ts
@@ -0,0 +1,13 @@
+import {auth} from "@clerk/nextjs/server"
+export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => {
+    const {userId} = await auth()
+    if(!userId) throw new Error("Unauthorized");
+
+    const params = new URLSearchParams({
+        clientId: process.env.AURINKO_CLIENT_ID as string,
+        serviceType,
+        scopes: 'Mail.Read Mail.ReadWrite Mail.Send Mail.Drafts Mail.All',
+        responseType: 'code',
+        returnUrl: `${process.env.NEXT_PUBLIC_URL}/api/aurinko/callback`
+    })
+}
